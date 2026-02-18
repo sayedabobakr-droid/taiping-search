0d5e5a8f564a9a51a57dee9001fa47fa3e5c6ce8
sayedabobakr-droid <sayed.abobakr@mediu.my>
2026-02-18 18:22:59 +0200

    Add files via upload

---
+++ google.js
@@ -0,0 +1,33 @@
+window.onload =function(){
+                const isloggedin =localStorage.getItem("isloggedin")
+                if(isloggedin!=="true"){
+                    window.location.href= "login.html";                
+                }
+            }
+            function logout(){
+                localStorage.removeItem("isloggedin");
+                window.location.href="login.html";
+            }
+    // بنستنى الصفحة تحمل بالكامل الأول
+    document.addEventListener('DOMContentLoaded', function() {
+        
+        const searchInput = document.getElementById('search');
+
+        // بنراقب الكتابة جوه الـ input
+        searchInput.addEventListener('keypress', function (e) {
+            // لو داس Enter
+            if (e.key === 'Enter') {
+                // بنشيل المسافات الزايدة ونحول النص
+                const word = searchInput.value.trim();
+
+                if (word === "اهلا") {
+                    window.location.href = "search/search1.html"; 
+                } else if (word !== "") {
+                    // لو كتب أي حاجة تانية يوديه لجوجل العادي مثلاً (اختياري)
+                    window.location.href = "https://www.google.com/search?q=" + encodeURIComponent(word);
+                } if(word ==="رمضان") {
+                    window.location.href ="search/search1.html";
+                }
+            }
+        });
+});
